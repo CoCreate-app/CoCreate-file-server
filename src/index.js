3feacd78fdd6265d0d5641484775721d7d823146
Frank Pagan <frank@cocreate.app>
2022-12-04 14:05:17 +0000

    fix: console.log to trace error in deployment

--- src/index.js
+++ src/index.js
@@ -33,9 +33,11 @@ class CoCreateFileSystem {
                 })
                 if (!organization || !organization.document[0])
                     return res.send('Organization cannot be found using the domain: ' + hostname + ' in platformDB: ' + masterOrg);
+                
+                console.log('file-server-----', organization.document[0], organization_id, organization_id.toString())
+
                 organization_id = organization.document[0]._id
             }
-            console.log('file-server-----', organization.document[0], organization_id, organization_id.toString())
             console.log('file-server-----', organization_id.toString())
 
             let [url, parameters] = req.url.split("?");
